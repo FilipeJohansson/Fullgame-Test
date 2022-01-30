82aedced7718dc052a28aad928dda1c0b7f1ebb9
Filipe Johansson <filipejohansson7@gmail.com>
2022-01-30 10:30:50 -0300

    Boss health bar

--- Assets/Scripts/Enemies/Boss/BossBase.cs
+++ Assets/Scripts/Enemies/Boss/BossBase.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossBase : EnemyBase {
+
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     public override void Start() {
         base.Start();
+
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
-    void Update() {
-
+    void FixedUpdate() {
+        healthBar.SetHealth(currentHealth);
     }
 }
